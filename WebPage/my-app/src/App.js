e5fae1b5707d02f5656ed279a2a927981ab04f9c
Eryk1997 <erykmati@o2.pl>
2020-03-22 10:20:33 +0100

    dodanie navbara/bootstrap

--- WebPage/my-app/src/App.js
+++ WebPage/my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Hello </h1>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
